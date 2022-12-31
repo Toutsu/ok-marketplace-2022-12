e099262a1db56eb79ba4114d5f9394b5cfffa2b1
Aleksej Salopaev <hegin4@gmail.com>
2022-12-31 14:18:40 +1000

    m1l1-quickstart first class and first test

---
+++ m1l1-quickstart/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    kotlin("jvm") version "1.7.21"
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test-junit"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
-
 rootProject.name = "ok-marketplace-2022-12"
+
+include("m1l1-quickstart")
 
